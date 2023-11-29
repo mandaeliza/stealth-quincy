f8ef34ff4804d886dcefabae6bebd31b7ee6249b
Mandy M. <127141046+mandaeliza@users.noreply.github.com>
2023-11-29 16:52:01 +0100

    Testing toggle button

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ fish.addEventListener("mouseover", function () {
 pets.addEventListener("mouseover", function () {
     pets.innerText = "20K";
 });
+
+var toggle = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var headline = document.querySelector("h1");
+var body = document.querySelector("body");
+
+toggle.addEventListener("click", function () {
+    if (toggle.classList.contains("light")) {
+        toggle.classList.remove("light");
+        ball.classList.remove("move-right");
+        headline.innerText = "Stealth Quincy";
+    } else {
+        body.classList.add("light");
+        ball.classList.add("move-right");
+        headline.innerText = "Party Quincy";
+    }
+})
